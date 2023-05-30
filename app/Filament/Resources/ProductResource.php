0ea0c5a2046ec8011b89335cd744f56e2f43f940
lutfahimayatus <lutfahimayatuss@gmail.com>
2023-05-31 06:32:44 +0700

    Komit pertama

---
+++ app/Filament/Resources/ProductResource.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProductResource\Pages;
+use App\Models\Product;
+use Filament\Forms\Components\Card;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ProductResource extends Resource
+{
+    protected static ?string $model = Product::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    protected static ?string $recordTitleAttribute = 'name';
+
+    public static function getModelLabel(): string
+    {
+        return 'Data Produk';
+    }
+
+    public static function getPluralModelLabel(): string
+    {
+        return 'Produk';
+    }
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Card::make([
+                    FileUpload::make('picture')
+                        ->label('Gambar Produk')
+                        ->image(),
+                ])
+                    ->columnSpanFull(),
+                Card::make(
+                    [
+                        TextInput::make('name')
+                            ->label('Nama')
+                            ->required()
+                            ->columnSpan(1),
+                        TextInput::make('price')
+                            ->label('Harga')
+                            ->minValue(1)
+                            ->numeric()
+                            ->mask(
+                                fn (TextInput\Mask $mask) => $mask
+                                    ->numeric()
+                                    ->decimalPlaces(2)
+                                    ->decimalSeparator('.')
+                                    ->integer()
+                                    ->mapToDecimalSeparator(['.'])
+                                    ->minValue(1)
+                                    ->normalizeZeros()
+                                    ->padFractionalZeros()
+                                    ->thousandsSeparator('.'),
+                            )
+                            ->required()
+                            ->columnSpan(1),
+                        TextInput::make('stock')
+                            ->label('Stok')
+                            ->numeric()
+                            ->minValue(0)
+                            ->required()
+                            ->columnSpanFull(),
+                        Textarea::make('description')
+                            ->label('Deskripsi')
+                            ->columnSpanFull(),
+
+                    ]
+                )->columnSpanFull()->columns(2),
+            ])->columns(3);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')->sortable()->searchable()->label('Nama'),
+                TextColumn::make('price')->sortable()->searchable()->money(currency: 'IDR', shouldConvert: true)->label('Harga'),
+                TextColumn::make('stock')->sortable()->label('Stok'),
+                TextColumn::make('created_at')->sortable()->hidden(),
+                TextColumn::make('description')->limit(20)->searchable()->label('Deskripsi'),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->defaultSort('created_at', 'desc')
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProducts::route('/'),
+            'create' => Pages\CreateProduct::route('/create'),
+            'edit' => Pages\EditProduct::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+}
---
+++ app/Http/Livewire/CustomLogin.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use DanHarrin\LivewireRateLimiting\Exceptions\TooManyRequestsException;
+use DanHarrin\LivewireRateLimiting\WithRateLimiting;
+use Filament\Facades\Filament;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Http\Livewire\Auth\Login as BasePage;
+use Filament\Http\Responses\Auth\Contracts\LoginResponse;
+use Illuminate\Validation\ValidationException;
+
+class CustomLogin extends BasePage
+{
+    use InteractsWithForms;
+    use WithRateLimiting;
+
+    public $username = '';
+
+    public $password = '';
+
+    public $remember = false;
+
+    public function mount(): void
+    {
+        if (Filament::auth()->check()) {
+            redirect()->intended(Filament::getUrl());
+        }
+
+        $this->form->fill();
+    }
+
+    public function authenticate(): ?LoginResponse
+    {
+        try {
+            $this->rateLimit(5);
+        } catch (TooManyRequestsException $exception) {
+            throw ValidationException::withMessages([
+                'username' => __('filament::login.messages.throttled', [
+                    'seconds' => $exception->secondsUntilAvailable,
+                    'minutes' => ceil($exception->secondsUntilAvailable / 60),
+                ]),
+            ]);
+        }
+
+        $data = $this->form->getState();
+
+        if (! Filament::auth()->attempt([
+            'username' => $data['username'],
+            'password' => $data['password'],
+        ], $data['remember'])) {
+            throw ValidationException::withMessages([
+                'username' => __('filament::login.messages.failed'),
+            ]);
+        }
+
+        session()->regenerate();
+
+        return app(LoginResponse::class);
+    }
+
+    protected function getFormSchema(): array
+    {
+        return [
+            TextInput::make('username')
+                ->label('Nama Pengguna')
+                ->required()
+                ->autocomplete(),
+            TextInput::make('password')
+                ->label(__('filament::login.fields.password.label'))
+                ->password()
+                ->required(),
+            Checkbox::make('remember')
+                ->label(__('filament::login.fields.remember.label')),
+        ];
+    }
+}
